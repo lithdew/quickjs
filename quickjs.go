aaa42285c9d2d101135484c60c41cc68e213b4c9
Kenta Iwasaki <kenta@lithdew.net>
2020-07-15 03:21:34 +0900

    quickjs: remove static flag

--- quickjs.go
+++ quickjs.go
@@ -14,7 +14,7 @@ import (
 #cgo CFLAGS: -D_GNU_SOURCE
 #cgo CFLAGS: -DCONFIG_BIGNUM
 #cgo CFLAGS: -fno-asynchronous-unwind-tables
-#cgo LDFLAGS: -lm -lpthread -static
+#cgo LDFLAGS: -lm -lpthread
 
 #include "bridge.h"
 */
